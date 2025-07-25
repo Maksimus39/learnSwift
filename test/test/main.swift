6558a6f63c5b9f3062a558c9863a0f2d3493969d
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-07-24 22:28:20 -0700

    learn commit

--- test/test/main.swift
+++ test/test/main.swift
@@ -8,3 +8,17 @@
 import Foundation
 
 print("Hello, World!")
+
+
+// comment
+
+
+// -------------------------------------- Имена и значения -----------------------------
+
+var lada = 10
+var ladA = "Priora"
+
+print(lada, ladA)
+
+lada = 20
+print(lada, ladA)
