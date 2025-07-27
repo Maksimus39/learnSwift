99c9086f8206b04c1198613b75775901c204d22b
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-07-26 18:05:05 -0700

    if ... else

--- Logical operators (if)/Logical operators (if)/main.swift
+++ Logical operators (if)/Logical operators (if)/main.swift
@@ -7,5 +7,95 @@
 
 import Foundation
 
-print("Hello, World!")
+//print("Введите имя:")
+//let name = readLine()
+//print("Введите ваш возраст")
+//let age = readLine()
+//if Int(age!)! >= 18 {
+//    print("\(name!) может уже садиться за рудь")
+//} else {
+//    print("\(name!), подрасти ещё немного!")
+//}
+
+// Задачи
+
+// 1
+
+//print("Введите два числа, и выведите в консоль сначала большее а потом меньшее число:")
+//
+//let numberOne = Int(readLine()!)!
+//let numberTwo = Int(readLine()!)!
+//
+//if numberOne > numberTwo {
+//    print(numberOne)
+//    print(numberTwo)
+//} else {
+//    print(numberTwo)
+//    print(numberOne)
+//}
+
+
+// 2
+
+//print("Введите год рождения")
+//let year = Int(readLine()!)!
+//let const = 1900
+//
+//if year < const {
+//    print("Не может такого быть!")
+//} else {
+//    print("Спасибо")
+//}
+
+// 3
+
+//print("Введите любимый язык программирования:")
+//let language = readLine()!
+//
+//if language == "Swift" || language == "SWIFT" {
+//    print("Класс, я тоже люблю \(language)")
+//} else {
+//    print("\(language) конечно не плох, но SWIFT лучше")
+//}
+
+
+// 4
+
+//print("Идёт ли дождь?")
+//let isRainy = readLine()!
+//
+//if isRainy == "да" {
+//    print("Не забудь зонтик")
+//} else {
+//    print("Cветит ли солнце?")
+//    let sun = readLine()!
+//    if sun == "да" {
+//        print("Не забудь солнечные очки")
+//    }else {
+//        print("Хорошей прогулки")
+//    }
+//}
+
+
+
+// 5
+
+//print("""
+//    Меню:
+//    1 - Радио
+//    2 - Телевизор
+//    3 - Отмена
+//    """)
+//
+//var ansver = readLine()
+//if ansver == "1" {print("Включите радио")}
+//else if ansver == "2" {print("Включите телевизор")}
+//else if ansver == "3" {print("Выход")}
+//else {print("Error")}
+
+
+
+
+
+
 
